d82dc8bd7bc3094ff3d93d707ac343d55b3dfb00
Jaryl <jarylolz12@gmail.com>
2020-07-28 22:12:08 +0800

    redeploymet for github repository

--- config/db.js
+++ config/db.js
@@ -1,11 +1,4 @@
 const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb+srv://jaryl:jaryl@resto.8cgyi.gcp.mongodb.net/resto?retryWrites=true&w=majority', {
-	// pangalan or pag create sa imong database
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	useCreateIndex: true
-});
 
 // const db = mongoose.connect('mongodb://localhost:27017/resto', {
 // 	// pangalan or pag create sa imong database
