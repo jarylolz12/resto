ee52318fe8f05956c6ff54a98e27ddb021d23a8e
Jaryl <jarylolz12@gmail.com>
2020-09-09 16:19:57 +0800

    pushing to heroku

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-// const PORT = 5000;
-// app.listen(PORT, () => console.log(`server started on http://127.0.0.1:${PORT}/`));
+const PORT = 5000;
+app.listen(PORT, () => console.log(`server started on http://127.0.0.1:${PORT}/`));
